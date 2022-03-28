adf41ad2627458a9562160bd7ba2512ee24c93c8
Giga Chubinidze <gchubinidze@unisens.ge>
2022-03-28 12:14:09 +0400

    fixed everything in this Task2 branch!

--- IdealWeight.rb
+++ IdealWeight.rb
@@ -1,10 +1,14 @@
-def calculate
-    puts "Enter Your Name"
-    name = gets.chomp
-    puts "Enter Your Height"
-    height = gets.chomp.to_i
-    weight = height - 110
-    puts height < 110 ? "Your Weight Is Already Optimal": "Your Ideal Weight Is: #{weight}"
-end
-
-calculate
+class HumanWeight
+    VALUE = 110
+    def calculate
+      puts "Enter Your Name"
+      name = gets.chomp
+      puts "Enter Your Height"
+      height = gets.chomp.to_i
+      weight = height - VALUE
+    return height < VALUE ? "Your Weight Is Already Optimal": "Your Ideal Weight Is: #{weight}"
+    end
+  end
+  
+  obj1 = HumanWeight.new.calculate
+  puts obj1
