356c88f41d8644eac7c801ece5cf0ecc0b843763
Giga-Chubinidze <100270368+Giga-Chubinidze@users.noreply.github.com>
2022-03-30 09:57:57 +0400

    Update AreaOfTriangle.rb

--- AreaOfTriangle.rb
+++ AreaOfTriangle.rb
@@ -1,9 +1,15 @@
 class Triangle
   VALUE = 0.5
-  def calculate base, height
-    VALUE*base*height
+  
+  def initialize base, height
+    @base = base
+    @height = height
+  end
+  
+  def calculate
+    VALUE * @base * @height
   end
 end
 
-obj1 = Triangle.new.calculate 10, 5
-puts obj1
+obj1 = Triangle.new(10, 5)
+puts obj1.calculate
