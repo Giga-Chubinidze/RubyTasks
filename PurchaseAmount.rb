c0efd0aec1db5d2f104bd663c1f1e4241b827f6d
Giga Chubinidze <gchubinidze@unisens.ge>
2022-03-31 16:44:41 +0400

    Added object creation, just run and youll see how it works

--- PurchaseAmount.rb
+++ PurchaseAmount.rb
@@ -3,12 +3,14 @@ class ShoppingCart
   def initialize
     @result = {}
     @result_string = []
+    @total_price = 0
   end
 
   def calculate
     console_input
     puts @result
     puts @result_string
+    puts "Total price: #{@total_price}"
   end
 
   def console_input
@@ -21,10 +23,12 @@ class ShoppingCart
       price = gets.chomp.to_f
       puts 'Enter quantity'
       quantity = gets.chomp.to_i
-      @result[name] = "Price: #{price}, Quantity: #{quantity}"
+      @result[name] = {"Price": price, "Quantity": quantity}
       @result_string << "Item: #{name}, Total Amount: #{price * quantity}"
+      @total_price += price * quantity
     end
   end
 end
 
-
+object1 = ShoppingCart.new
+object1.calculate
