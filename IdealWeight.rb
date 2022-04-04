2d0861806572779046e0b08ba2675dc4cd5fb65d
Giga Chubinidze <gchubinidze@unisens.ge>
2022-04-04 10:03:33 +0400

    fixed input last time, please accept it

--- IdealWeight.rb
+++ IdealWeight.rb
@@ -1,17 +1,26 @@
 class HumanWeight
   VALUE = 110
-  
+
   def initialize
-    puts "Enter Your Name"
+    puts 'Enter Your Name'
     @name = gets.chomp
-    puts "Enter Your Height"
-    @height = gets.chomp.to_i
+    puts 'Enter Your Height'
+    @height = gets.chomp
   end
-  
+
   def calculate
+    check_string(@height)
+    @height = @height.to_i
     @weight = @height - VALUE
-    @height < VALUE ? "#{@name}, Your Weight Is Already Optimal": 
-                      "#{@name}, Your Ideal Weight Is: #{@weight}"
+    if @height < VALUE
+      "#{@name}, Your Weight Is Already Optimal"
+    else
+      "#{@name}, Your Ideal Weight Is: #{@weight}"
+    end
+  end
+
+  def check_string(string)
+    abort('Wrong input') unless string.scan(/\D/).empty?
   end
 end
 
