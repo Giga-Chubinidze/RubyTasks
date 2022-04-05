132e1f46f93befaffec8b82aa9a2d8b9eac1c37d
Giga Chubinidze <gchubinidze@unisens.ge>
2022-04-05 10:18:04 +0400

    Service functions added

---
+++ AreaOfTriangleService.rb
@@ -0,0 +1,38 @@
+VALUE = 0.5
+
+class Triangle
+  class GigasIOError < StandardError; end
+
+  def initialize(base, height)
+    @base = base
+    @height = height
+  end
+
+  def call(method_name)
+    input_check
+    method(method_name).call
+    rescue GigasIOError => e
+      warn e
+  end 
+
+  private 
+
+  def area
+    VALUE * @base * @height
+  end
+
+  def circumference
+    # a test method to check the functionality
+    "i am circumference"
+  end
+
+  def input_check
+    if @base != 0 || @height != 0
+      raise GigasIOError, "Invalid Input"
+    end
+  end
+end
+
+obj1 = Triangle.new(10, 0)
+puts obj1.call(:area)
+
