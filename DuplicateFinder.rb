580de83e6fb76597be164f2fb7e5a17435444f55
Giga Chubinidze <gchubinidze@unisens.ge>
2022-04-06 17:00:22 +0400

    finished

---
+++ DuplicateFinder.rb
@@ -0,0 +1,39 @@
+class DuplicateFinder
+  class GigasIOError < StandardError; end
+
+  def initialize(list)
+    @list = list
+  end
+
+  def call
+    input_validation(@list)
+    duplicate_elements
+  rescue GigasIOError => e
+    warn e
+  end
+
+  private
+
+  def duplicate_elements
+    (@list.find_all do |e| @list.count(e) > 1 end).uniq.to_s
+  end
+
+  def input_validation(obj)
+    obj.each do |elem|
+      raise GigasIOError, 'Invalid Input' unless elem.to_i.to_s == elem && obj.length > 1
+    end
+  end
+end
+
+## ----------------------------------------------------- TESTING
+
+numbers = []
+loop do
+  input = gets.chomp
+  break if input == 's' # s for stopping the loop!
+
+  numbers << input
+end
+
+obj1 = DuplicateFinder.new(numbers)
+puts obj1.call
