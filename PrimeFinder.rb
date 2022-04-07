c2fffbfe4a9c474dc59bc55c8c23af3929fbf3d6
Giga Chubinidze <gchubinidze@unisens.ge>
2022-04-07 10:28:01 +0400

    recursively solved it

---
+++ PrimeFinder.rb
@@ -0,0 +1,46 @@
+class PrimeFinder
+  class GigasIOError < StandardError; end
+
+  def initialize(array)
+    @array = array
+  end
+
+  def call
+    input_validation
+    prime_counter
+  rescue GigasIOError => e
+    warn e
+  end
+
+  private
+
+  def prime_counter
+    prime_num = 0
+    @array.each do |i|
+      prime_num += 1 if is_prime?(i)
+    end
+    prime_num
+  end
+
+  def is_prime?(num, div = 2)
+    if num <= 2
+      return false if num == 1
+      return true if num == 2 else false
+    end
+    return false if (num % div).zero?
+    return true if div * div > num
+
+    is_prime?(num, div + 1)
+  end
+
+  def input_validation
+    unless @array.all? do |i|
+      i.is_a? Integer
+    end
+      raise GigasIOError, 'Invalid Input'
+    end
+  end
+end
+
+obj1 = PrimeFinder.new([1, 2, 3, 4, 5, 6, 7, 8, 9])
+puts obj1.call
