9ddbf9e53a1f687a78c4550a39f078f1ac9fa3b1
Giga Chubinidze <gchubinidze@unisens.ge>
2022-05-04 16:33:44 +0400

    added csv read and write in trie code as functions

--- CSVWriter/to_csv.rb
+++ CSVWriter/to_csv.rb
@@ -1,16 +1,20 @@
 require 'csv'
-def write_file(hash)
-  CSV.open("CSVWriter/data.csv", "a") do |csv|
-    hash.to_a.each do |pair|
-      csv << pair
-    end
+
+class FileWriter
+  attr_accessor :path
+
+  def initialize
+    @path = 'CSVWriter/data.csv'
   end
-  puts "data loaded to csv successfully!"
+
+  def save_to_csv(word_list)
+    CSV.open(path, 'a') do |csv|
+      csv << word_list
+    end
+    puts 'Successfully added to CSV'
+  end
+
+  def read_from_csv
+    CSV.read(path).to_a
+  end
 end
-
-
-hash = {1 => "one",2 => "two",3 => "three",4 => "four"}
-write_file(hash)
-
-
-
